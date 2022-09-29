daf78600e4bfa779f63dffa1476398bfb1640f2e
João Gustavo Soares Bispo <83095574+jgbispo@users.noreply.github.com>
2022-09-29 13:21:42 +0000

    Initial commit

---
+++ src/imports.js
@@ -0,0 +1,12 @@
+// Convert
+export const convertDiv = document.getElementById("bin-converter")
+export const numberConvert = document.getElementById("input-convert")
+export const optionConvert = document.getElementById("types")
+export const convertedArea = document.getElementById("converted-area")
+
+// Calc
+export const calcDiv = document.getElementById("bin-calc")
+export const numberOneCalc = document.getElementById("input-calc-one")
+export const numberTwoCalc = document.getElementById("input-calc-two")
+export const optionCalc = document.getElementById("signals")
+export const calcArea = document.getElementById("calc-area")
